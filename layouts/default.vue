4259bb9f24f5382648e4dff00723a08e52ceb2de
Administrator <627157746@qq.com>
2020-06-11 17:47:43 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,121 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <div class="pa-2">
+        <v-img src="http://qukanbook.com/_nuxt/img/2e29f6f.png" />
+      </div>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-apps</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-switch
+                v-model="$vuetify.theme.dark"
+                primary
+                label="夜间模式"
+              />
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title class="hidden-xs-only" v-text="title" />
+      <v-spacer />
+      <v-row
+        align="center"
+        style="max-width: 650px"
+      >
+        <v-text-field
+          placeholder="请输入关键字。。。"
+          clearable
+          append-icon="mdi-magnify"
+          hide-details
+          @click:append="serach"
+        />
+      </v-row>
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      class="hidden-xs-only"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: '首页',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '菜单1',
+          to: '/menu1'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '菜单2',
+          to: '/menu2'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '菜单3',
+          to: '/menu3'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  methods: {
+    serach () {
+      console.log('abc')
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-row justify="center" align="start">
+    <v-col v-for="index in 24" :key="index" md="4" sm="6" xs="12">
+      <v-hover>
+        <template v-slot="{ hover }">
+          <v-card :elevation="hover ? 24 : 6">
+            <nuxt-link to="/">
+              <v-img
+                eager
+                lazy-src="https://bad.src/not/valid"
+                min-height="400"
+                min-width="250"
+                max-height="1000"
+                max-width="700"
+                class="grey lighten-2"
+                src="https://picsum.photos/350/165?random"
+              >
+                <div class="img-title white--text">
+                  <div class="mx-2 text-truncate" to="/">
+                    的说法就是看到了附件是的尖峰时刻劳动法的说法就是看到了附件是的尖峰时刻劳动法
+                  </div>
+                  <div class="float-left mx-2">
+                    2020-6-11
+                  </div>
+                  <div class="float-right mx-2">
+                    53张
+                  </div>
+                </div>
+                <template v-slot:placeholder>
+                  <v-row
+                    class="fill-height ma-0"
+                    align="center"
+                    justify="center"
+                  >
+                    <v-progress-circular indeterminate color="grey lighten-5" />
+                  </v-row>
+                </template>
+              </v-img>
+            </nuxt-link>
+          </v-card>
+        </template>
+      </v-hover>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+<style lang="scss" scope>
+a {
+  text-decoration: none;
+}
+.img-title{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-image: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.8) 0%,
+    transparent 72px
+  );
+}
+</style>
